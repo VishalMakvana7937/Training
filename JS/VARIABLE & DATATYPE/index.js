627c185aff33921246a6abc0be2531ac14fc9003
VishalMakvana7937 <vishalmakvana7937@gmail.com>
2024-12-21 06:04:16 +0530

    javascript

---
+++ JS/VARIABLE & DATATYPE/index.js
@@ -0,0 +1,39 @@
+// javascript variables
+// 1. let
+// 2. const
+// 3. var
+
+// var
+var age = 85;
+
+if (true) {
+    console.log(age);
+}
+
+function solve() {
+    var age = 25;
+    console.log(age);
+}
+
+solve();
+
+// let
+{
+    let a = 5;
+    console.log(a);
+}
+
+// const
+const xyz = 40;
+console.log(xyz);
+
+// JavaScript has 8 Datatypes
+
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Object
+// 7. Symbol
+// 8. BigInt
