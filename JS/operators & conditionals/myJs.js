143367d81562bdbd1ea3db8ae3a856535212d837
VishalMakvana7937 <vishalmakvana7937@gmail.com>
2024-12-27 06:01:41 +0530

    javascript

---
+++ JS/operators & conditionals/myJs.js
@@ -0,0 +1,18 @@
+
+
+console.log("operators & conditionals");
+
+// Arithmetic Operator
+let a = 2;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+console.log("..", a++);
+console.log("...", ++a);
+
+
