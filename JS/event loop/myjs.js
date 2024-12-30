17d90643d361e63befba707565f69ec612ba12e9
VishalMakvana7937 <vishalmakvana7937@gmail.com>
2024-12-30 23:06:47 +0530

    javascript

--- JS/event loop/myjs.js
+++ JS/event loop/myjs.js
@@ -6,3 +6,16 @@
 // call stack
 // browser
 // callback queue
+
+
+// sync
+console.log("Start.!");
+
+// async
+setTimeout(() => {
+    console.log('Love babbar');
+
+}, 5000);
+
+// sync
+console.log("End.!");
