9c91412a47a6c7bc2072618e532aa5e9a56f8557
VishalMakvana7937 <vishalmakvana7937@gmail.com>
2025-01-18 05:59:36 +0530

    react pastes view a pastes

--- react/react project create paste/src/components/Pastes.jsx
+++ react/react project create paste/src/components/Pastes.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Pastes = () => {
+
+  const pastes = useSelector((state) => state.paste.pastes);
+  console.log(pastes);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const dispatch = useDispatch();
+
+  const filterData = pastes.filter(
+    (paste) => paste.title.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
+
   return (
-    <div>Pastes</div>
+    <div>
+      <input className='p-1 rounded-2xl mt-2 w-[550px] pl-4' type="search" placeholder='Search here' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+
+      <div className='flex flex-col gap-5 mt-5'>
+        {
+          filterData.length > 0 &&
+          filterData.map((paste, index) => {
+            return (
+              <div key={index} className='border'>
+                <h2 className='text-lg font-bold'>{paste.title}</h2>
+                <p>{paste.content}</p>
+
+                <div className='flex flex-row place-content-between'>
+                  <button>
+                    Edit
+                  </button>
+                  <button>
+                    View
+                  </button>
+                  <button>
+                    Delete
+                  </button>
+                  <button>
+                    Edit
+                  </button>
+                  <button>
+                    Edit
+                  </button>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
   )
 }
 
